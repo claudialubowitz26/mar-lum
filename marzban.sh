f561a5b2dc8fd599d072e8dc0f74335493fc375b
claudialubowitz26 <139770192+claudialubowitz26@users.noreply.github.com>
2024-07-06 02:05:46 +0700

    Update marzban.sh

--- marzban.sh
+++ marzban.sh
@@ -42,7 +42,6 @@ if [ -f /etc/os-release ]; then
     fi
 fi
 apt install sudo curl -y
-clear
 if [ "$supported_os" != true ]; then
     colorized_echo red "Error: Skrip ini hanya support di Debian 11 dan Ubuntu 20.04. Mohon gunakan OS yang di support."
     exit 1
@@ -111,7 +110,7 @@ fi
 mkdir -p /etc/data
 
 #domain
-clear
+
 # Fungsi untuk mendapatkan IP dari domain
 get_domain_ip() {
     local domain=$1
@@ -125,7 +124,7 @@ input_domain() {
     domain=$(cat /etc/data/domain)
     echo "$domain"
 }
-
+clear
 current_ip=$(curl -s https://ipinfo.io/ip)
 if [ -z "$current_ip" ]; then
     echo "Tidak dapat menemukan IP publik saat ini."

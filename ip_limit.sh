13d6d6105f7ffcb51610f837d0d430c869acd6de
claudialubowitz26 <139770192+claudialubowitz26@users.noreply.github.com>
2024-07-06 11:29:07 +0700

    Update ip_limit.sh

--- ip_limit.sh
+++ ip_limit.sh
@@ -19,6 +19,83 @@ function LOGI() {
     echo -e "${green}[INF] $* ${plain}"
 }
 
+# check root
+[[ $EUID -ne 0 ]] && LOGE "ERROR: You must be root to run this script! \n" && exit 1
+
+# Check OS and set release variable
+if [[ -f /etc/os-release ]]; then
+    source /etc/os-release
+    release=$ID
+elif [[ -f /usr/lib/os-release ]]; then
+    source /usr/lib/os-release
+    release=$ID
+else
+    echo "Failed to check the system OS, please contact the author!" >&2
+    exit 1
+fi
+
+echo "The OS release is: $release"
+
+os_version=""
+os_version=$(grep -i version_id /etc/os-release | cut -d \" -f2 | cut -d . -f1)
+
+if [[ "${release}" == "arch" ]]; then
+    echo "Your OS is Arch Linux"
+elif [[ "${release}" == "parch" ]]; then
+    echo "Your OS is Parch linux"
+elif [[ "${release}" == "manjaro" ]]; then
+    echo "Your OS is Manjaro"
+elif [[ "${release}" == "armbian" ]]; then
+    echo "Your OS is Armbian"
+elif [[ "${release}" == "opensuse-tumbleweed" ]]; then
+    echo "Your OS is OpenSUSE Tumbleweed"
+elif [[ "${release}" == "centos" ]]; then
+    if [[ ${os_version} -lt 8 ]]; then
+        echo -e "${red} Please use CentOS 8 or higher ${plain}\n" && exit 1
+    fi
+elif [[ "${release}" == "ubuntu" ]]; then
+    if [[ ${os_version} -lt 20 ]]; then
+        echo -e "${red} Please use Ubuntu 20 or higher version!${plain}\n" && exit 1
+    fi
+elif [[ "${release}" == "fedora" ]]; then
+    if [[ ${os_version} -lt 36 ]]; then
+        echo -e "${red} Please use Fedora 36 or higher version!${plain}\n" && exit 1
+    fi
+elif [[ "${release}" == "debian" ]]; then
+    if [[ ${os_version} -lt 11 ]]; then
+        echo -e "${red} Please use Debian 11 or higher ${plain}\n" && exit 1
+    fi
+elif [[ "${release}" == "almalinux" ]]; then
+    if [[ ${os_version} -lt 9 ]]; then
+        echo -e "${red} Please use AlmaLinux 9 or higher ${plain}\n" && exit 1
+    fi
+elif [[ "${release}" == "rocky" ]]; then
+    if [[ ${os_version} -lt 9 ]]; then
+        echo -e "${red} Please use Rocky Linux 9 or higher ${plain}\n" && exit 1
+    fi
+elif [[ "${release}" == "oracle" ]]; then
+    if [[ ${os_version} -lt 8 ]]; then
+        echo -e "${red} Please use Oracle Linux 8 or higher ${plain}\n" && exit 1
+    fi
+else
+    echo -e "${red}Your operating system is not supported by this script.${plain}\n"
+    echo "Please ensure you are using one of the following supported operating systems:"
+    echo "- Ubuntu 20.04+"
+    echo "- Debian 11+"
+    echo "- CentOS 8+"
+    echo "- Fedora 36+"
+    echo "- Arch Linux"
+    echo "- Parch Linux"
+    echo "- Manjaro"
+    echo "- Armbian"
+    echo "- AlmaLinux 9+"
+    echo "- Rocky Linux 9+"
+    echo "- Oracle Linux 8+"
+    echo "- OpenSUSE Tumbleweed"
+    exit 1
+
+fi
+
 # Variables
 log_folder="${XUI_LOG_FOLDER:=/var/log}"
 iplimit_log_path="${log_folder}/3xipl.log"
@@ -123,56 +200,67 @@ EOF
 
 # Install IP Limit function
 install_iplimit() {
-    # Check the OS and install necessary packages
-    case "${release}" in
-    ubuntu)
-        apt update
-        if [[ "${os_version}" -ge 20 ]]; then
-            apt install python3-pip -y
-            python3 -m pip install pyasynchat --break-system-packages
-        fi
-        apt install fail2ban -y
-        ;;
-    debian | armbian)
-        apt update && apt install fail2ban -y
-        ;;
-    centos | almalinux | rocky | oracle)
-        yum update -y && yum install epel-release -y
-        yum -y install fail2ban
-        ;;
-    fedora)
-        dnf -y update && dnf -y install fail2ban
-        ;;
-    arch | manjaro | parch)
-        pacman -Syu --noconfirm fail2ban
-        ;;
-    *)
-        echo -e "${red}Unsupported operating system. Please check the script and install the necessary packages manually.${plain}\n"
-        exit 1
-        ;;
-    esac
-
     if ! command -v fail2ban-client &>/dev/null; then
-        echo -e "${red}Fail2ban installation failed.${plain}\n"
-        exit 1
-    fi
+        echo -e "${green}Fail2ban is not installed. Installing now...!${plain}\n"
 
-    echo -e "${green}Fail2ban installed successfully!${plain}\n"
+        # Check the OS and install necessary packages
+        case "${release}" in
+        ubuntu)
+            if [[ "${os_version}" -ge 24 ]]; then
+                apt update && apt install python3-pip -y
+                python3 -m pip install pyasynchat --break-system-packages
+            fi
+            apt update && apt install fail2ban -y
+            ;;
+        debian | armbian)
+            apt update && apt install fail2ban -y
+            ;;
+        centos | almalinux | rocky | oracle)
+            yum update -y && yum install epel-release -y
+            yum -y install fail2ban
+            ;;
+        fedora)
+            dnf -y update && dnf -y install fail2ban
+            ;;
+        arch | manjaro | parch)
+            pacman -Syu --noconfirm fail2ban
+            ;;
+        *)
+            echo -e "${red}Unsupported operating system. Please check the script and install the necessary packages manually.${plain}\n"
+            exit 1
+            ;;
+        esac
+
+        if ! command -v fail2ban-client &>/dev/null; then
+            echo -e "${red}Fail2ban installation failed.${plain}\n"
+            exit 1
+        fi
+
+        echo -e "${green}Fail2ban installed successfully!${plain}\n"
+    else
+        echo -e "${yellow}Fail2ban is already installed.${plain}\n"
+    fi
 
     echo -e "${green}Configuring IP Limit...${plain}\n"
 
+    # make sure there's no conflict for jail files
     iplimit_remove_conflicts
 
+    # Check if log file exists
     if ! test -f "${iplimit_banned_log_path}"; then
         touch ${iplimit_banned_log_path}
     fi
 
+    # Check if service log file exists so fail2ban won't return error
     if ! test -f "${iplimit_log_path}"; then
         touch ${iplimit_log_path}
     fi
 
+    # Create the iplimit jail files
+    # we didn't pass the bantime here to use the default value
     create_iplimit_jails
 
+    # Launching fail2ban
     if ! systemctl is-active --quiet fail2ban; then
         systemctl start fail2ban
         systemctl enable fail2ban
@@ -182,9 +270,9 @@ install_iplimit() {
     systemctl enable fail2ban
 
     echo -e "${green}IP Limit installed and configured successfully!${plain}\n"
+    before_show_menu
 }
 
-# Remove IP Limit function
 remove_iplimit() {
     echo -e "${green}\t1.${plain} Only remove IP Limit configurations"
     echo -e "${green}\t2.${plain} Uninstall Fail2ban and IP Limit"
@@ -197,6 +285,7 @@ remove_iplimit() {
         rm -f /etc/fail2ban/jail.d/3x-ipl.conf
         systemctl restart fail2ban
         echo -e "${green}IP Limit removed successfully!${plain}\n"
+        before_show_menu
         ;;
     2)
         rm -rf /etc/fail2ban
@@ -224,6 +313,7 @@ remove_iplimit() {
             ;;
         esac
         echo -e "${green}Fail2ban and IP Limit removed successfully!${plain}\n"
+        before_show_menu
         ;;
     0)
         echo -e "${yellow}Cancelled.${plain}\n"

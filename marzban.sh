fb6a5076ed5a9e52181a2a65c439b3f28f0643a8
claudialubowitz26 <139770192+claudialubowitz26@users.noreply.github.com>
2024-08-06 11:18:54 +0700

    Update marzban.sh

--- marzban.sh
+++ marzban.sh
@@ -387,7 +387,7 @@ http
         ssl_certificate /var/lib/marzban/certs/$domain/fullchain.cer;
         ssl_certificate_key /var/lib/marzban/certs/$domain/privkey.key;
 
-        ssl_protocols TLSv1.2 TLSv1.3;
+        ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;
         ssl_prefer_server_ciphers on;
         ssl_ciphers EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5;
 
